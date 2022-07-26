1945635bab62d0360f283679315d6595fc7fd0d7
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-07-26 16:21:18 +0200

    updates

--- docker/kuka_fri_install.sh
+++ docker/kuka_fri_install.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
 echo "[----------> SpaceVecAlg <------------------]"
-cd 
+cd
 git clone --recursive https://github.com/costashatz/SpaceVecAlg.git
 cd SpaceVecAlg
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SIMD=ON -DPYTHON_BINDING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=native -faligned-new" -DPYTHON_BINDING=OFF ..
 make -j$(nproc)
 make install
 
 echo "[----------> RBDyn <------------------]"
-cd 
+cd
 git clone --recursive https://github.com/costashatz/RBDyn.git
 cd RBDyn
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SIMD=ON -DPYTHON_BINDING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=native -faligned-new" -DPYTHON_BINDING=OFF ..
 make -j$(nproc)
 make install
 
 echo "[----------> mc_rbdyn_urdf <------------------]"
-cd 
+cd
 git clone --recursive https://github.com/costashatz/mc_rbdyn_urdf.git
 cd mc_rbdyn_urdf
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SIMD=ON -DPYTHON_BINDING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=native -faligned-new" -DPYTHON_BINDING=OFF ..
 make -j$(nproc)
 make install
 
 echo "[----------> corrade <------------------]"
-cd 
+cd
 git clone https://github.com/mosra/corrade.git
 cd corrade
+git checkout 0d149ee9f26a6e35c30b1b44f281b272397842f5
 mkdir build && cd build
 cmake ..
 make -j$(nproc)
 make install
 
 echo "[----------> robot_controllers <------------------]"
-cd 
+cd
 git clone https://github.com/epfl-lasa/robot_controllers.git
 cd robot_controllers
 mkdir build && cd build
